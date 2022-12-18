63d1ee5d2ecf136a243e9349dd0f5674c9be0081
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-12-19 07:21:59 +0900

    Ch30 커스텀 뷰와 터치 이벤트
    터치 이벤트 처리하기
    (BoxDrawingView 구현하기)

--- app/src/main/java/silbajuk/ch30/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/silbajuk/ch30/draganddraw/BoxDrawingView.kt
@@ -1,9 +1,35 @@
 package silbajuk.ch30.draganddraw
 
 import android.content.Context
+import android.graphics.PointF
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import java.util.jar.Attributes
 
+private const val TAG = "BoxDrawingView"
+
 class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val current = PointF(event.x, event.y)
+        var action = ""
+        when(event.action){
+            MotionEvent.ACTION_DOWN->{
+                action = "ACTION_DOWN"
+            }
+            MotionEvent.ACTION_MOVE->{
+                action = "ACTION_MOVE"
+            }
+            MotionEvent.ACTION_UP->{
+                action = "ACTION_UP"
+            }
+            MotionEvent.ACTION_CANCEL->{
+                action = "ACTION_CANCEL"
+            }
+        }
+        Log.i(TAG, "$action at x = ${current.x}, y = ${current.y}")
+        return true
+    }
+
 }
