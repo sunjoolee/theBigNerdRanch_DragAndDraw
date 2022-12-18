c3b8fa997b1326e82428e235843cc269a0ac5e99
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-12-19 07:47:05 +0900

    Ch30 커스텀 뷰와 터치 이벤트
    모션 이벤트 추척하기
    (Box클래스 추가하기)
    (Box 객체를 사용하는 코드 추가하기)

--- app/src/main/java/silbajuk/ch30/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/silbajuk/ch30/draganddraw/BoxDrawingView.kt
@@ -11,25 +11,43 @@ import java.util.jar.Attributes
 private const val TAG = "BoxDrawingView"
 
 class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+
+    private var currentBox: Box? = null
+    private val boxen = mutableListOf<Box>()
+
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val current = PointF(event.x, event.y)
         var action = ""
         when(event.action){
             MotionEvent.ACTION_DOWN->{
                 action = "ACTION_DOWN"
+                //그리기 상태를 재설정
+                currentBox = Box(current).also{
+                    boxen.add(it)
+                }
             }
             MotionEvent.ACTION_MOVE->{
                 action = "ACTION_MOVE"
+                updateCurrentBox(current)
             }
             MotionEvent.ACTION_UP->{
                 action = "ACTION_UP"
+                updateCurrentBox(current)
+                currentBox = null
             }
             MotionEvent.ACTION_CANCEL->{
                 action = "ACTION_CANCEL"
+                currentBox = null
             }
         }
         Log.i(TAG, "$action at x = ${current.x}, y = ${current.y}")
         return true
     }
 
+    private fun updateCurrentBox(current : PointF){
+        currentBox?.let{
+            it.end = current
+            invalidate()
+        }
+    }
 }
