2eb3a824844ed1934af5008167fe2018d830ebfb
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-12-19 07:09:08 +0900

    Ch30 커스텀 뷰와 터치 이벤트
    DragAndDraw 프로젝트 설정하기
    BoxDrawingView 생성하기
    (BoxDrawingView의 초기 구현)
    (BoxDrawingView를 레이아웃에 추가하기)

---
+++ app/src/main/java/silbajuk/ch30/draganddraw/BoxDrawingView.kt
@@ -0,0 +1,9 @@
+package silbajuk.ch30.draganddraw
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import java.util.jar.Attributes
+
+class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+}
---
+++ app/src/main/java/silbajuk/ch30/draganddraw/DrawActivity.kt
@@ -0,0 +1,11 @@
+package silbajuk.ch30.draganddraw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DrawActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drag_and_draw)
+    }
+}
